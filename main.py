9dc1ab9e9a8527d18c78a721a352bc6a007cfbc3
fanbrawla <blokhinart@mail.ru>
2022-11-17 17:56:22 +0700

    Реализовал построение дерева по массиву

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class tree:
-    def __init__(self, data = None, name = None):
+    def __init__(self, data = 0, name = None):
         self.name = name
         self.left = None
         self.right = None
@@ -12,12 +12,21 @@ class tree:
         return new
 
     def PrintTree(self):
-        print(self.data)
+        print(self.data, self.name)
         if self.left:
+            print("l")
             self.left.PrintTree()
         if self.right:
+            print("r")
             self.right.PrintTree()
 
+    def Cost(self, cost = 0):
+        cost += self.data
+        if self.left:
+            cost = self.left.Cost(cost)
+        if self.right:
+            cost = self.right.Cost(cost)
+        return cost
 
 def sort(count):
     for i in range(len(count)-1):
@@ -44,14 +53,44 @@ def ini():
     sort(count)
     return(count)
 
+def MasToTree(count):
+    tr = []
+    haftr = []
+    for i in range(len(count)):
+        a = tree(count[i][1], count[i][0])
+        tr.append(a)
+    while len(tr) > 1:
+        cost = tr[0].Cost()
+        l = 0
+        for j in range(len(tr)):
+            if (cost > tr[j].Cost()):
+                l = j
+        r = 0
+        if l == 0:
+            r = 1
+        cost1 = tr[r].Cost()
+        for k in range(len(tr)):
+            if ((k!=l) & (cost1 > tr[k].Cost())):
+                r = k
+        a = tr[l].add(tr[r])
+        tr.append(a) 
+        if r > l:
+            del tr[r]
+            del tr[l]
+        else:
+            del tr[l]
+            del tr[r]
+    return tr
+
 def main():
     count = ini()
     sort(count)
-    haf = tree(count[len(count)-1][1], count[len(count)-1][0])
-    tr = tree(2, "r")
-    a = haf.add(tr)
-    a.PrintTree()
-
+    print(count)
+    tre = MasToTree(count)
+    print(f"len = {len(tre)}")
+    for i in range(len(tre)):
+        tre[i].PrintTree()
+        print(f"i = {i}")
 
 if __name__ == "__main__":
     	main()
