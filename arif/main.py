99c59055e83edba4c068ef40b3634465eff91ba1
fanbrawla <blokhinart@mail.ru>
2022-12-18 15:28:41 +0700

    Заношу символ/кол-во в будущий закодированный файл

--- arif/main.py
+++ arif/main.py
@@ -1,17 +1,29 @@
 def main():
-    sum_slovar = 0
     f = input("Введите название файла для кодирования:")
     with open(f'{f}', 'r') as fp:
-        test_sum = 0
+        sum_wlist = 0
         chunk = fp.read(1)
-        slovar = {}
+        wlist = {}
         while chunk:
-            test_sum += 1
-            if slovar.get(chunk) == None:
-                slovar.update({chunk: 1})
+            sum_wlist += 1
+            if wlist.get(chunk) == None:
+                wlist.update({chunk: 1})
             else:
-                slovar[chunk] = slovar[chunk] + 1
+                wlist[chunk] = wlist[chunk] + 1
             chunk = fp.read(1)
+
+    sorted_wlist = dict(sorted(wlist.items(), key=lambda item: item[1], reverse=True)) 
+    wlist_mas = [0, 1]
+    for i in sorted_wlist:
+        wlist_mas.append(sorted_wlist[i] + wlist_mas[-1])
+
+    f = open(f"{f}.enc", "wb+")
+    print(len(sorted_wlist))
+    f.write(len(sorted_wlist).to_bytes(1, "little"))
+    for i in sorted_wlist:
+        f.write(i.encode("ascii"))
+        f.write(sorted_wlist[i].to_bytes(4, "little"))
+    print(sorted_wlist)
 
 if __name__ == "__main__":
     	main()
